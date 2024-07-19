9bf5193c9ebd61613c0a67958a259ddcdacc1450
Sinned Angel <sinned.angel@gmail.com>
2024-07-19 22:12:05 +0700

    Update splash animation

--- lib/splash/splash_page.dart
+++ lib/splash/splash_page.dart
@@ -5,21 +5,43 @@ class SplashPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
         body: Center(
             child: Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Image(
-          image: AssetImage('assets/simply.jpeg'),
-          width: 200,
-        ),
-        Text('Simply POS',
-            style: TextStyle(
-              fontSize: 50,
-              fontWeight: FontWeight.bold,
-              color: Colors.blueGrey,
-            ))
+        TweenAnimationBuilder(
+          tween: Tween<double>(begin: 0, end: 1),
+          duration: const Duration(milliseconds: 500),
+          child: const Image(
+            image: AssetImage('assets/simply.png'),
+            width: 200,
+          ),
+          builder: (context, value, child) => Opacity(
+            opacity: 1,
+            child: Padding(
+              padding: EdgeInsets.only(bottom: value * 20),
+              child: child,
+            ),
+          ),
+        ), 
+        TweenAnimationBuilder(
+          tween: Tween<double>(begin: 0, end: 1),
+          duration: const Duration(milliseconds: 1000),
+          child: const Text('Simply POS',
+              style: TextStyle(
+                fontSize: 50,
+                fontWeight: FontWeight.bold,
+                color: Colors.blueGrey,
+              )),
+          builder: (context, value, child) => Opacity(
+            opacity: value,
+            child: Padding(
+              padding: EdgeInsets.only(top: value * 20),
+              child: child,
+            ),
+          ),
+        )
       ],
     )));
   }
