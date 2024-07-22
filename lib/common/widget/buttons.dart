f793f9c925e95805d4196a6ad516396b76cd602d
Sinned Angel <sinned.angel@gmail.com>
2024-07-22 21:54:53 +0700

    update primary button

--- lib/common/widget/buttons.dart
+++ lib/common/widget/buttons.dart
@@ -3,30 +3,41 @@ import 'package:simply_pos/common/theme/pallete.dart';
 
 class PrimaryButton extends StatelessWidget {
   final String buttonText;
-  const PrimaryButton({
-    super.key,
-    this.buttonText = '',
-  });
+  final Function() onPressed;
+  final double width;
+  final double height;
+  const PrimaryButton(
+      {super.key,
+      this.buttonText = '',
+      required this.onPressed,
+      this.width = 200,
+      this.height = 50});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(8),
-          color: AppPallete.secondayColor),
-      child: ElevatedButton(
-        style: ElevatedButton.styleFrom(
-          fixedSize: const Size(200, 50),
-        ),
-        onPressed: () {},
-        child: Text(
-          buttonText,
-          style: const TextStyle(
-            fontSize: 17,
-            fontWeight: FontWeight.w600,
+    return Row(
+      children: [
+        Expanded(
+          child: Container(
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                color: AppPallete.secondayColor),
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                fixedSize: Size(width, height),
+              ),
+              onPressed: onPressed,
+              child: Text(
+                buttonText,
+                style: const TextStyle(
+                  fontSize: 17,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
           ),
         ),
-      ),
+      ],
     );
   }
 }
